eb3a0150ee0456ed0a12a552cc2151682f45d073
Patrick Min <makereallyawesome@gmail.com>
2018-02-15 15:30:13 +0900

    connect react-django

--- config/settings/base.py
+++ config/settings/base.py
@@ -55,6 +55,7 @@ THIRD_PARTY_APPS = [
     'taggit_serializer', # Serializer for tags
     'rest_auth', # rest auth for social login
     'rest_auth.registration', # registration for social login
+    'corsheaders', # To accept requests from React
 ]
 
 # Apps specific for this project go here.
@@ -74,6 +75,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -197,6 +199,7 @@ STATIC_URL = '/static/'
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
+    str(ROOT_DIR.path('frontend', 'build', 'static')),
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
@@ -292,3 +295,8 @@ REST_FRAMEWORK = {
 
 REST_USE_JWT = True # rest-auth를 JWT와 함께 사용할 것임
 ACCOUNT_LOGOUT_ON_GET = True # logout의 HTTP method를 POST -> GET 변경
+
+SOCIALACCOUNT_QUERY_EMAIL = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token # JWT
+from minstagram import views
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
@@ -22,6 +23,8 @@ urlpatterns = [
     url(r'^images/', include('minstagram.images.urls', namespace='images')),
     # Notifications
     url(r'^notifications/', include('minstagram.notifications.urls', namespace='notifications')),
+    # catch-all-url
+    url(r'^', views.ReactAppView.as_view()),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
