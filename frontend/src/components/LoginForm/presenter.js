d93c5c5f123ff0a3cc0ea924f80259675d5e40e7
Patrick Min <makereallyawesome@gmail.com>
2018-04-10 22:29:35 +0900

    loginform restructed, i18n

---
+++ frontend/src/components/LoginForm/presenter.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Ionicon from "react-ionicons";
+import formStyles from "components/shared/formStyles.scss";
+
+const LoginForm = (props, context) => (
+  <div className={formStyles.formComponent}>
+      <form className={formStyles.form} onSubmit={props.handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="Username"
+            className={formStyles.textInput} 
+            value={props.usernameValue}
+            onChange={props.handleInputChange}
+            name="username"
+          />
+          <input 
+            type="password"
+            placeholder="Password"
+            className={formStyles.textInput}
+            value={props.passwordValue}
+            onChange={props.handleInputChange}
+            name="password"
+          />
+          <input 
+            type="submit"
+            placeholder="Log in"
+            className={formStyles.button}
+          />
+      </form>
+      <span className={formStyles.divider}>or</span>
+      <span className={formStyles.facebookLink}>
+          <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" />
+          Log in with Facebook
+      </span>
+      <span className={formStyles.forgotLink}>Forgot password?</span>
+  </div>
+);
+
+LoginForm.propTypes = {
+    usernameValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handleInputChange: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired
+}
+
+LoginForm.contextTypes = {
+  t: PropTypes.func.isRequired
+}
+
+export default LoginForm;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,16 @@
 export const translations = {
-    es: {
-        "Login": "Iniciar Sesión",
-    }
-};
+  'ko': {
+    'About Us': '만든 사람들',
+    'Supports': '고객지원',
+    'Blog': '블로그',
+    'Press': '언론보도',
+    'API': 'API',
+    'Jobs': '채용',
+    'Privacy': '개인정보',
+    'Terms': '용어',
+    'Directory': '디렉토리',
+    'Language': '언어',
+  },
+  'options': {
+  },
+}
